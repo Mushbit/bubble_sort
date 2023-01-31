224e016de5931214a74ef3c8b7d4c66e70d56ee5
Max <mushbit@protonmail.com>
2023-01-31 11:50:33 +0100

    Added a basic loop to not have to nest. Added interactivity by ways of printed instruction and input cappable of adding (n) values

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,37 @@
 def bubble_sort(array)
+    
     place_holder = 0
+    counter = 0
     i = 0
-    while i < array.length
+
+    loop do
+
         if array[i] > array[i + 1]
-            p array[i]
-            p array
             place_holder = array[i]
             array[i] = array[i + 1]
             array[i + 1] = place_holder
+            counter += 1
         end
+
+        i += 1
+
+        break if !(array[i + 1]) && (counter == 0)
+        unless array[i + 1]
+            i = 0
+            counter = 0
+        end
+
     end
+
     array
 end
 
-bubble_sort([4,3,78,2,0,2])
+puts "Please enter a set of numbers you want to have ordered in the numarical fashion.
+When finished, type: run"
+numbers = []
+until numbers[numbers.length - 1] == "run"
+    numbers.push(gets.chomp)
+end
+numbers.pop
+numbers.map!(&:to_i)
+p bubble_sort(numbers)
